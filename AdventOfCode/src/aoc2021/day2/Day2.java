b1dd261e5353f0a98ad2fe307ebcd1b4d7db0322
Maurits Hameter <maurits-hameter@hotmail.com>
2021-12-02 06:24:55 +0100

    Solution day 2.
    Also added a template for a new day

--- AdventOfCode/src/aoc2021/day2/Day2.java
+++ AdventOfCode/src/aoc2021/day2/Day2.java
@@ -7,11 +7,46 @@ import java.util.ArrayList;
 public class Day2 {
 
     public static void run() {
-        int res = 0;
+        int posX = 0;
+        int posY = 0;
+
+        int aim = 0;
 
         System.out.println("Solutions day 2:");
 
-        ArrayList<Integer> input = InputLoader.toIntegerList("input/day2.txt");
-        //ArrayList<String> input = InputLoader.toStringList("input/day2.txt");
+        ArrayList<String> input = InputLoader.toStringList("input/day2.txt");
+
+        for(String s : input) {
+            if (s.contains("forward")) {
+                int val = Integer.parseInt(s.substring(8));
+                posX += val;
+            } else if (s.contains("down")) {
+                int val = Integer.parseInt(s.substring(5));
+                posY += val;
+            } else if (s.contains("up")) {
+                int val = Integer.parseInt(s.substring(3));
+                posY -= val;
+            }
+        }
+
+        System.out.println("first answer: " + posX*posY);
+
+        posX = 0;
+        posY = 0;
+
+        for(String s : input) {
+            if (s.contains("forward")) {
+                int val = Integer.parseInt(s.substring(8));
+                posX += val;
+                posY += aim*val;
+            } else if (s.contains("down")) {
+                int val = Integer.parseInt(s.substring(5));
+                aim += val;
+            } else if (s.contains("up")) {
+                int val = Integer.parseInt(s.substring(3));
+                aim -= val;
+            }
+        }
+        System.out.println("second answer: " + posX*posY);
     }
 }
---
+++ AdventOfCode/src/aoc2021/dayTemplate/DayTemplate.java
@@ -0,0 +1,17 @@
+package aoc2021.dayTemplate;
+
+import aoc2021.utilities.InputLoader;
+
+import java.util.ArrayList;
+
+public class DayTemplate {
+
+    public static void run() {
+        int res = 0;
+
+        System.out.println("Solutions day x:");
+
+        ArrayList<Integer> input = InputLoader.toIntegerList("input/dayX.txt");
+        //ArrayList<String> input = InputLoader.toStringList("input/dayX.txt");
+    }
+}
