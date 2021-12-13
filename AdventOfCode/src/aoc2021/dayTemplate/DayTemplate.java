783a8110900b31ecca0581eccdd04b9f9a8da38b
Maurits Hameter <maurits-hameter@hotmail.com>
2021-12-13 08:23:12 +0100

    Changed DayTemplate so that it has 2 standard lines for output part 1 and part 2 of the puzzle

--- AdventOfCode/src/aoc2021/dayTemplate/DayTemplate.java
+++ AdventOfCode/src/aoc2021/dayTemplate/DayTemplate.java
@@ -7,11 +7,17 @@ import java.util.ArrayList;
 public class DayTemplate {
 
     public static void run() {
-        int res = 0;
+        int res1 = 0;
+        int res2 = 0;
 
         System.out.println("Solutions day x:");
 
         ArrayList<Integer> input = InputLoader.toIntegerList("input/dayX.txt");
         //ArrayList<String> input = InputLoader.toStringList("input/dayX.txt");
+
+
+
+        System.out.println("part 1: " + res1);
+        System.out.println("part 2: " + res2);
     }
 }
